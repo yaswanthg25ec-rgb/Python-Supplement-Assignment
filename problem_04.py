d04146e29915670c9b6befeca135bf2672c48160
yaswanthg25ec-rgb <yaswanth.g_25ec@kgkite.ac.in>
2026-01-30 11:49:16 +0530

    Update problem_04.py

--- problem_04.py
+++ problem_04.py
@@ -3,7 +3,7 @@
 
 numbers = [45, 12, 78, 34, 89, 23]
 largest = numbers[0]
-for i in range(len(numbers)):
+for i in range(1,len(numbers)):
     if numbers[i] > largest:
         largest = numbers[i]
 print(f"Largest number is: {largest}")
