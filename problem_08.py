e74be0693cee9ba4c39934c9d9f620e1df09cb1c
yaswanthg25ec-rgb <yaswanth.g_25ec@kgkite.ac.in>
2026-01-30 13:25:39 +0530

    Update problem_08.py

--- problem_08.py
+++ problem_08.py
@@ -2,10 +2,7 @@
 # Find and fix the error
 
 def is_palindrome(text):
+    text = text.lower()
     return text == text[::-1]
 
-word = "Racecar"
-if is_palindrome(word):
-    print(f"{word} is a palindrome")
-else:
-    print(f"{word} is not a palindrome")
+word="racecar"
